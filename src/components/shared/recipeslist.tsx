8a4f5be843b2e209bd68819114e5dc9093d6d888
jonatan-arn <jonatanarnandis@hotmail.es>
2023-03-27 00:24:22 +0200

    recipelist key

--- src/components/shared/recipeslist.tsx
+++ src/components/shared/recipeslist.tsx
@@ -8,8 +8,8 @@ interface recipelistProps {
 export default function recipeslist({ list }: recipelistProps) {
   return (
     <div className="grid grid-cols-1 gap-7 px-4 pt-3 lg:grid-cols-3">
-      {list.map((recipe, index) => (
-        <Recipecard key={index} recipe={recipe}></Recipecard>
+      {list.map((recipe) => (
+        <Recipecard key={recipe.id} recipe={recipe}></Recipecard>
       ))}
     </div>
   );
