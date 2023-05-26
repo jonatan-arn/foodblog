cb1b6aca7be93eb03fb14211372f4612510b10b5
jonatan-arn <jonatanarnandis@hotmail.es>
2023-05-26 15:04:00 +0200

    curry cacahuete

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,9 +1,11 @@
 import chanamasala from "/recipeimage/chanamasala.png";
 import carababurrata from "/recipeimage/carababurrata.png";
+import currycacahuete from "/recipeimage/currycacahuete.png";
 
 const images = new Map<string, string>();
 images.set("chanamasala.png", chanamasala);
 images.set("carababurrata.png", carababurrata);
+images.set("currycacahuete.png", currycacahuete);
 
 export const getImage = (name: string) => {
   return images.get(name);
